d820435616615de99b7adb93509883e4aac8539e
Evan <evanamiesgalonski@gmail.com>
2020-04-12 12:50:05 -0700

    clean up

---
+++ R/demo.R
@@ -0,0 +1,13 @@
+#' double
+#'
+#' @param num The number to be doubles
+#' @return The value for 'num', doubled
+#' @export
+#'
+#' @examples
+#' double(2)
+#'
+double <- function(num){
+  result <- num * 2
+  result
+}
--- tests/testthat/test-chk.R
+++
@@ -1,5 +0,0 @@
-test_that("chk_function template", {
-  expect_null(chk_function_template(""))
-  expect_invisible(chk_function_template(""))
-  chk::expect_chk_error(chk_function_template(1), "^`1` must be a character[.]$")
-})
