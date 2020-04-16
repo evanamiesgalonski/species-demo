81ad19c8ff2abadf0f5eea383ca280d29e516feb
Evan <evanamiesgalonski@gmail.com>
2020-04-15 20:33:02 -0700

    add conservation status fun

---
+++ R/species.R
@@ -0,0 +1,14 @@
+#' conservation_status
+#'
+#' @param species the species to provide
+#' @return
+#' @export
+#'
+#' @examples
+#' conservation_status("Anemone occidentalis - Carex nigricans")
+#'
+conservation_status <- function(species) {
+  species_conservation <- speciesdemo::bc_species[speciesdemo::bc_species$ScientificName == species,]
+  species_conservation_columns <- species_conservation[c("BCList", "COSEWIC Status", "Implemented Date")]
+  return(print(species_conservation_columns))
+}
